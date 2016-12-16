81e7516f92c2558ce7cfbef860bb747dbe08390f
conni <c.buschka@arcusx.com>
2016-12-16 14:15:35 +0100

    Removed container name from packaging container.

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -10,6 +10,5 @@ docker build --tag mechanic-build-$pkg .
 cd $PROJECT_DIR
 docker run --rm \
 	-e FIX_OWNER_UID=$UID \
-	--name mechanic-build-$pkg \
 	-v ${PROJECT_DIR}:/build \
 	mechanic-build-$pkg /build/packaging/$pkg/build.sh $PACKAGE_VERSION
