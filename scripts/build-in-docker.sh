75bc0c2d56a9863b7410e30e6ab0217d9bf2a492
conni <c.buschka@arcusx.com>
2017-01-27 08:56:16 +0100

    Moved docker container config for build container into sub dir.

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -15,7 +15,7 @@
 PROJECT_DIR=$(cd `dirname $0`/..; pwd)
 
 cd $PROJECT_DIR
-docker build -t mechanic-build .
+docker build -t mechanic-build -f $PROJECT_DIR/build-container/Dockerfile $PROJECT_DIR/build-container
 docker run --rm \
 	-e FIX_UID=$(id --user $USER) \
 	-e FIX_GID=$(id --group $USER) \
