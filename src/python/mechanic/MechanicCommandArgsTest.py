997fc01469da87b9c471a52b67e852b169e5f2aa
conni <c.buschka@arcusx.com>
2017-01-29 07:50:07 +0100

    Added test for -v.

--- src/python/mechanic/MechanicCommandArgsTest.py
+++ src/python/mechanic/MechanicCommandArgsTest.py
@@ -8,8 +8,18 @@ class MechanicCommandArgsTest(unittest.TestCase):
     def setUp(self):
       self.args = MechanicCommandArgs(["-v", "sub", "--sub-flag=sub_val"])
 
+    def givenArgsOf(self, args):
+      self.args = MechanicCommandArgs(args)
+
     def testCommandNameRecognized(self):
       self.assertEqual(self.args.commandName, "sub")
+
+    def testVerboseTrueRecognized(self):
+      self.assertTrue(self.args.verbose)
+
+    def testVerboseFalseRecognized(self):
+      self.givenArgsOf(["sub"])
+      self.assertFalse(self.args.verbose)
 
     def tearDown(self):
       self.args = None
