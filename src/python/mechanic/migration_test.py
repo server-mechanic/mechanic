82bd0c7dd89e2520f8d0c512accdd0d3932d2d6c
conni <c.buschka@arcusx.com>
2017-01-29 19:15:53 +0100

    Added test for Migration.

--- src/python/mechanic/migration_test.py
+++ src/python/mechanic/migration_test.py
@@ -6,5 +6,14 @@ from migration import Migration
 
 class MigrationTest(unittest.TestCase):
     def setUp(self):
-      self.migration = Migration()
+      self.migration = Migration(41, "file", "name", "start", "end", "status")
+
+    def testPropertiesAssigned(self):
+      self.assertEqual(41, self.migration.id)
+      self.assertEqual("file", self.migration.file)
+      self.assertEqual("name", self.migration.name)
+      self.assertEqual("start", self.migration.startTime)
+      self.assertEqual("end", self.migration.endTime)
+      self.assertEqual("status", self.migration.status)
+
 
