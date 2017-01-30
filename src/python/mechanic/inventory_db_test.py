4292a9c236c58b73653063169ce88c6aedce2944
conni <c.buschka@arcusx.com>
2017-01-30 08:19:55 +0100

    Added test for InventoryDb.listMigrations.

--- src/python/mechanic/inventory_db_test.py
+++ src/python/mechanic/inventory_db_test.py
@@ -22,6 +22,27 @@ class InventoryDbTest(unittest.TestCase):
       self.db.markMigrationAsSucceeded("test")
       self.assertTrue(self.db.hasMigrationSucceeded("test"))
 
+    def testListMigrationsWhenEmpty(self):
+      migrations = self.db.listMigrations("id")
+      self.assertEqual([], migrations)
+
+    def testListMigrationsAfterOneStarted(self):
+      self.db.markMigrationAsStarted("test")
+      migrations = self.db.listMigrations("id")
+      self.assertEqual(1, len(migrations))
+      self.assertEqual(1, migrations[0].id)
+      self.assertEqual("test", migrations[0].name)
+      self.assertEqual("STARTED", migrations[0].status)
+
+    def testListMigrationsAfterOneFinished(self):
+      self.db.markMigrationAsStarted("test")
+      self.db.markMigrationAsSucceeded("test")
+      migrations = self.db.listMigrations("start_date")
+      self.assertEqual(1, len(migrations))
+      self.assertEqual(1, migrations[0].id)
+      self.assertEqual("test", migrations[0].name)
+      self.assertEqual("SUCCESS", migrations[0].status)
+
     def tearDown(self):
       self.db.close()
       self.db = None
