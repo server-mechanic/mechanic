64a7c79e82b99847ccc65b5bb53aab527109d2f8
conni <c.buschka@arcusx.com>
2017-01-31 08:33:01 +0100

    Added test for context.py.

--- src/python/mechanic/context_test.py
+++ src/python/mechanic/context_test.py
@@ -8,3 +8,17 @@ class MechanicTest(unittest.TestCase):
     def setUp(self):
       self.context = Mechanic()
 
+    def testLoggerInitialized(self):
+      self.assertIsNotNone(self.context.logger)
+
+    def testConfigInitialized(self):
+      self.assertIsNotNone(self.context.config)
+
+    def testCommandsInitialized(self):
+      self.assertIsNotNone(self.context.commands)
+
+    def testDefaultCommandInitialized(self):
+      self.assertIsNotNone(self.context.defaultCommand)
+
+    def testInventoryInitialized(self):
+      self.assertIsNotNone(self.context.inventory)
