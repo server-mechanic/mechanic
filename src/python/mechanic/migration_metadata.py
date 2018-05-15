7bb999d9c50ec6850de38c96cb1d8faa693e661a
conni <c.buschka@arcusx.com>
2018-05-15 08:37:15 +0200

    Added isRepeatable() to migration metadata.

--- src/python/mechanic/migration_metadata.py
+++ src/python/mechanic/migration_metadata.py
@@ -4,5 +4,10 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 class MigrationMetadata(object):
-  def __init__(self,props):
+  PROP_REPEATABLE = "mechanic-migration-repeatable"
+
+  def __init__(self, props):
     self.props = props
+
+  def isRepeatable(self):
+    return self.props.get(MigrationMetadata.PROP_REPEATABLE) == True
---
+++ src/python/mechanic/migration_metadata_test.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import unittest
+from mechanic.migration_metadata import MigrationMetadata
+
+class MigrationMetadataTest(unittest.TestCase):
+    def testRepeatable(self):
+      metadata = MigrationMetadata(props={MigrationMetadata.PROP_REPEATABLE: True})
+      self.assertEqual(metadata.isRepeatable(), True)
